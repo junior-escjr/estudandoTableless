4f625d8e4405aeeba25ffcb00c742fb8f2d155a9
juniorbdb <junior_escjr@yahoo.com.br>
2015-06-12 11:41:45 -0300

    Atualização - fim da aula 1 - html, xhtml e praticas de seo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pro Tableless</title>
+	<meta name="description" content="Descrição da página">
+	<meta name="keywords" content="Palavras chaves separadas por vírgula">
+</head>
+<body>
+
+<!-- rel="nofollow" = Serve para caso você queira indicar um link para seu cliente e o Google não indexa-lo, pois caso este link não seja relacionado com o contexto do site perderá pontos no Google -->
+
+	Aula de Tableless, acesse o link da 
+	<a target="_blank" title="Site da Vx Comunicação" rel="nofollow" href="http://www.vxcomunicacao.com.br">Vx Comunicação</a>.
+</body>
+</html>
